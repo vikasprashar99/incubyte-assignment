1b0d606a7b687317e5ceef5e853190d2673214e0
Vikas Prashar <vikasprashar99@gmail.com>
2025-08-31 11:23:35 +0530

    test: Add test for newline delimiter

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -12,4 +12,8 @@ describe("String Calculator", () => {
   test("should return the sum of comma-separated numbers", () => {
     expect(add("1,5")).toBe(6);
   });
+
+  test("should handle new lines between numbers", () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
 });
