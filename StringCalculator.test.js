79de83dd8325db7ba49972b4ccfdf48270e53d29
Vikas Prashar <vikasprashar99@gmail.com>
2025-08-31 11:21:14 +0530

    test: Add test for single number

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -1,7 +1,11 @@
-const { add } = require('./StringCalculator');
+const { add } = require("./StringCalculator");
 
-describe('String Calculator', () => {
-    test('should return 0 for an empty string', () => {
-        expect(add("")).toBe(0);
-    });
+describe("String Calculator", () => {
+  test("should return 0 for an empty string", () => {
+    expect(add("")).toBe(0);
+  });
+
+  test("should return the number itself for a single number", () => {
+    expect(add("1")).toBe(1);
+  });
 });
