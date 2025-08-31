5a0685cf64b18e88f0649407ffc169b1d84f8108
Vikas Prashar <vikasprashar99@gmail.com>
2025-08-31 11:24:13 +0530

    test: Add test for custom delimiter

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -16,4 +16,8 @@ describe("String Calculator", () => {
   test("should handle new lines between numbers", () => {
     expect(add("1\n2,3")).toBe(6);
   });
+
+  test("should support a custom delimiter", () => {
+    expect(add("//;\n1;2")).toBe(3);
+  });
 });
